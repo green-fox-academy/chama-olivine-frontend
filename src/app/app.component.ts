0c75181d6e2b5d71f7360c8181fe584f37509df3
dyanimay <53790185+dyanimay@users.noreply.github.com>
2020-01-30 10:46:06 +0100

    create character button and error message are colored. train has been deleted (#35)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
-    const audio = new Audio();
-    audio.src = './assets/foo.mp3';
-    audio.load();
-    audio.play();
-  }
-}
+export class AppComponent { }
